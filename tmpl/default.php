a7bf0051c5a195351d2386fcfbae6b7666ad395b
thomas@winterling-labs.com <thomas@winterling-labs.com>
2018-11-20 15:39:18 +0100

    add stylesheet and javascript

---
+++ tmpl/default.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package     mod_dd-slider
+ * @author      Thomas Winterling
+ * @email       info@winterling-labs.com
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+JHtml::_('stylesheet', 'mod_dd_slider/style.css', array('version' => 'auto', 'relative' => true));
+JHtml::_('script', 'mod_dd_slider/article_slides.js', array('version' => 'auto', 'relative' => true));
+
+?>
