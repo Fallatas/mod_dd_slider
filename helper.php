436e6b7896b46b3dec8bff68b5d534c6a80305a3
thomas@winterling-labs.com <thomas@winterling-labs.com>
2018-11-21 14:08:14 +0100

    getSliderHTML function changed

--- helper.php
+++ helper.php
@@ -34,37 +34,7 @@ class modDD_SliderHelper
 
         $moduleparams = (json_decode($db->loadResult()));
 
-        $html = '<div id="dd_article_slides">';
-
-        $i = 0;
-
-        foreach ($moduleparams->params as $slide)
-        {
-
-            $i++;
-            $html .= '<div id="dd_article_slides_fields' . $i . '" class="article_slide">';
-            $html .=    '<div class="article_slide_inner">';
-            $html .=        '<img src="' . $slide->image . '" alt="'. $slide->image_alt .'">';
-            $html .=        '<div class="article_slide_info">';
-            $html .=            '<p>' . $slide->image_desc .'</p>';
-            $html .=            '<small>Bild: '. $slide->image_source . '</small>';
-            $html .=        '</div>';
-            $html .=        '<button class="article_slide_info_toggle">';
-            $html .=            '<span class="icon-dd-arrow-up"></span>';
-            $html .=            '<span class="icon-dd-arrow-down"></span>';
-            $html .=        '</button>';
-            $html .=    '</div>';
-            $html .= '</div>';
-        }
-
-        $html .= '<div class="article_slides_controls">';
-        $html .=     '<span class="icon-dd-arrow-left" id="slide-left"></span>';
-        $html .=     '<span id="article_slide_active">1</span> / <span>'. count( (array) $moduleparams->params) .'</span>';
-        $html .=     '<span class="icon-dd-arrow-right" id="slide-right"></span>';
-        $html .= '</div>';
-
-
-        return $html;
+        return $moduleparams;
     }
 
 }
