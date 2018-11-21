a0ce52a779cb80b8f9a19d42d170b58b10b4a652
thomas@winterling-labs.com <thomas@winterling-labs.com>
2018-11-21 14:14:49 +0100

    sliderfunction fixed

--- tmpl/default.php
+++ tmpl/default.php
@@ -13,4 +13,41 @@ JHtml::_('script', 'mod_dd_slider/article_slides.js', array('version' => 'auto',
 
 ?>
 
-<?php echo $sliderfunction; ?>
+<?php
+
+?>
+
+<div id="dd_article_slides">
+
+    <?php
+
+    $i = 0;
+
+    foreach ($sliderfunction->params as $slide)
+    {
+        $i++;
+        ?>
+        <div id="dd_article_slides_fields<?php echo $i;?>" class="article_slide">
+            <div class="article_slide_inner">
+                <img src="<?php echo $slide->image?>" alt="<?php echo $slide->image_alt?>">
+                <div class="article_slide_info">
+                    <p><?php echo $slide->image_desc?></p>
+                    <small>Bild: <?php echo $slide->image_source?></small>
+                </div>
+                <button class="article_slide_info_toggle">
+                    <span class="icon-dd-arrow-up"></span>
+                    <span class="icon-dd-arrow-down"></span>
+                </button>
+            </div>
+        </div>
+        <?php
+    }
+    ?>
+
+    <div class="article_slides_controls">
+        <span class="icon-dd-arrow-left" id="slide-left"></span>
+        <span id="article_slide_active">1</span> / <span><?php echo count( (array) $sliderfunction->params)?></span>
+        <span class="icon-dd-arrow-right" id="slide-right"></span>
+    </div>
+
+</div>
